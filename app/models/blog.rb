afb863000e3eb34868921a69cb561a0e2787510b
Greg Vissing <greg.vissing@gmail.com>
2017-06-25 05:16:50 +0000

    Update blog model

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,9 +2,4 @@ class Blog < ApplicationRecord
     enum status: { draft: 0, published: 1 }
     extend FriendlyId
     friendly_id :title, use: :slugged
-<<<<<<< HEAD
-    
-    validates_presence_of :title, :body
-=======
->>>>>>> 084bcd7a6cccb29e73250777ff0cc22f56cfb701
 end
