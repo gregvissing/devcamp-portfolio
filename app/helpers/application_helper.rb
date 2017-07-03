46da924a3687f01de7fbb7c7947a9d4e6361b96d
Greg Vissing <greg.vissing@gmail.com>
2017-07-03 17:46:13 +0000

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
             content_tag(:p, greeting, class: "source-greeting")
         end 
     end
+    
+    def copyright_generator
+        VissingViewTool::Renderer.copyright 'Greg Vissing', 'All rights reserved'
+    end
 end
